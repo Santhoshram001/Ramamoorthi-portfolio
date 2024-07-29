52e5189bd313bcdbc8cb66c0e8a7ff2a3d226d2d
Ramamoorthi S <santhoshramu2021@gmail.com>
2024-07-29 18:41:22 +0530

    .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -43,7 +43,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/Ramamoorthi-r-v-371b29295/"
+                href="https://www.linkedin.com/in/ramamoorthis/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -87,7 +87,7 @@ function Home2() {
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/Ramamoorthi-r-v-371b29295/"
+                  href="https://www.linkedin.com/in/ramamoorthis/"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour  home-social-icons"
